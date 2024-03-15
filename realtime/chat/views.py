2635fc96aaf009724cc7fb6946bc0d245712a8c4
Collins David Obiene <obienecollins@gmail.com>
2024-03-15 20:52:26 +0100

    Added Authentication Permission

--- realtime/chat/views.py
+++ realtime/chat/views.py
@@ -1,11 +1,13 @@
 # from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from .models import CustomUser, Profile, ChatRoom, Message, Participant
 from .serializers import ProfileSerializer, ChatRoomSerializer, MessageSerializer, ParticipantSerializer, UserSerializer
 # Create your views here.
 
 class UserSignUp(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
 class ProfileListCreate(generics.ListCreateAPIView):
     queryset = Profile.objects.all()
