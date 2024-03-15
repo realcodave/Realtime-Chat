fb5f25cd6fe0e0d6f59972604d00dd243d1495c4
Collins David Obiene <obienecollins@gmail.com>
2024-03-15 20:34:38 +0100

    Adding models to admin

--- realtime/chat/admin.py
+++ realtime/chat/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import Participant, Profile, ChatRoom, Message, CustomUser
+
+admin.site.register(CustomUser)
+admin.site.register(Participant)
+admin.site.register(Profile)
+admin.site.register(ChatRoom)
+admin.site.register(Message)
